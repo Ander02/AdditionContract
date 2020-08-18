a92e462a3e6480ce60a56ab07ca0c7477fab0480
Anderson Pessoa <Anderson Pessoa>
2020-08-18 09:54:41 -0300

    init

---
+++ contracts/Addition.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: <SPDX-License>
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Addition {
+  int public sum = 0;
+
+  function add (int x, int y) public {
+    sum = x + y;
+  }
+}
+
